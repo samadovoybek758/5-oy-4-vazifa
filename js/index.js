43ae4e0cf04f4ecfb92ad16fb4312ed00627e313
samadovoybek758 <oybek22samadov@gmail.com>
2024-07-10 15:39:10 +0500

    firstly

---
+++ js/index.js
@@ -0,0 +1,419 @@
+const body = document.getElementById('body');
+body.style.backgroundColor = '#EBEBEB'
+
+
+
+const contin = document.getElementById('continer')
+contin.style.maxWidth = "1203px";
+contin.style.marginRight = "auto";
+contin.style.marginLeft = "auto";
+
+
+
+
+
+
+// navbar qismi
+
+const nav = document.getElementById("navbar");
+
+nav.style.display= 'flex';
+nav.style.justifyContent = 'space-between';
+nav.style.alignItems = 'center';
+nav.style.margin = '0 30px 0 30px'
+
+
+
+
+
+
+const nav_ul = document.getElementById("navbar_ul");
+
+nav_ul.style.display = "flex";
+nav_ul.style.gap = "20px";
+nav_ul.style.listStyle = "none";
+
+
+const nav_link1 = document.getElementById("navbar_link1")
+
+nav_link1.style.textDecoration = "none";
+nav_link1.style.color = "#2B2B2B";
+nav_link1.style.fontSize = "16px";
+nav_link1.style.fontWeight = "600";
+nav_link1.style.fontFamily = "Nunito Sans";
+
+
+const nav_link2 = document.getElementById("navbar_link2")
+
+nav_link2.style.textDecoration = "none";
+nav_link2.style.color = "#2B2B2B";
+nav_link2.style.fontSize = "16px";
+nav_link2.style.fontWeight = "600";
+nav_link2.style.fontFamily = "Nunito Sans";
+
+
+
+
+const nav_link3 = document.getElementById("navbar_link3")
+
+nav_link3.style.textDecoration = "none";
+nav_link3.style.textDecoration = "none";
+nav_link3.style.color = "#2B2B2B";
+nav_link3.style.fontSize = "16px";
+nav_link3.style.fontWeight = "600";
+nav_link3.style.fontFamily = "Nunito Sans";
+
+
+
+
+
+
+
+const nav_btn = document.getElementById('buttons1_btn1');
+
+nav_btn.style.padding = '11px 25px';
+nav_btn.style.fontSize = '18px';
+nav_btn.style.color = 'white';
+nav_btn.style.fontFamily = "Nunito Sans";
+nav_btn.style.backgroundColor = '#423F3E';
+nav_btn.style.borderRadius = '5px';
+nav_btn.style.border = 'none';
+
+
+
+const nav_btn2 = document.getElementById('buttons1_btn2');
+
+nav_btn2.style.padding = '11px 25px';
+nav_btn2.style.fontSize = '18px';
+nav_btn2.style.color = 'white';
+nav_btn2.style.fontFamily = "Nunito Sans";
+nav_btn2.style.backgroundColor = '#423F3E';
+nav_btn2.style.borderRadius = '5px';
+nav_btn2.style.border = 'none';
+nav_btn2.style.marginLeft = '20px';
+
+
+
+
+
+
+
+
+// header qismi
+const head = document.getElementById('header');
+
+head.style.maxWidth = '960px';
+head.style.textAlign = 'center';
+head.style.marginLeft = '98px';
+head.style.marginTop = '220px';
+head.style.marginBottom = '307px'
+
+
+const head_h1 = document.getElementById('header_h1');
+
+head_h1.style.fontSize = '64px';
+head_h1.style.fontFamily = 'Nunito Sans';
+head_h1.style.color = '#2B2B2B';
+head_h1.style.marginBottom = '31px';
+
+
+const head_p = document.getElementById('header_p');
+
+head_p.style.fontSize ='24px';
+head_p.style.fontWeight ='700';
+head_p.style.color ='#2B2B2B';
+head_p.style.fontFamily ='Nunito Sans';
+
+
+
+
+
+
+
+// main qismi
+
+const main = document.getElementById('main');
+
+main.style.display = 'flex';
+main.style.alignItems = 'center';
+main.style.gap = '68px';
+main.style.marginBottom = '100px'
+
+
+const main_h1 = document.getElementById('main_h1');
+
+main_h1.style.fontSize = '53px';
+main_h1.style.color = '#2B2B2B';
+main_h1.style.fontFamily = 'Nunito Sans';
+main_h1.style.marginBottom = '60px';
+
+
+const main_tag = document.getElementById('main_tagline');
+
+main_tag.style.display = "flex";
+main_tag.style.gap = "54px";
+
+const main_p1 = document.getElementById('main_p1');
+
+main_p1.style.color = '#2B2B2B';
+main_p1.style.fontSize = '16px';
+main_p1.style.fontWeight = '600';
+main_p1.style.fontFamily = 'Nunito Sans';
+main_p1.style.margin = '0';
+
+
+const main_p2 = document.getElementById('main_p2');
+
+main_p2.style.color = '#2B2B2B';
+main_p2.style.fontSize = '16px';
+main_p2.style.fontWeight = '600';
+main_p2.style.fontFamily = 'Nunito Sans';
+main_p2.style.margin = '0';
+
+
+
+const main_read1 = document.getElementById('main_read1');
+
+main_read1.style.display = 'flex';
+main_read1.style.gap = '10px';
+
+
+const main_read2 = document.getElementById('main_read2');
+
+main_read2.style.display = 'flex';
+main_read2.style.gap = '10px';
+
+
+
+
+
+
+
+
+
+// hero qismi
+
+const hero = document.getElementById('hero');
+
+hero.style.display = 'flex';
+hero.style.gap = '225.999px';
+hero.style.alignItems = 'center';
+hero.style.marginBottom = '145px';
+
+
+
+
+const hero_h3 = document.getElementById('hero_nulla');
+
+hero_h3.style.fontSize = '39px';
+hero_h3.style.color = '#2B2B2B';
+hero_h3.style.fontFamily = 'Nunito Sans';
+
+
+const hero_p = document.getElementById("hero_p");
+
+hero_p.style.fontSize = '18px';
+hero_p.style.fontFamily = 'Nunito Sans';
+hero_p.style.color = '#2B2B2B';
+hero_p.style.maxWidth = '508px';
+
+
+
+const hero_uls = document.getElementById('hero_uls');
+
+hero_uls.style.display = "flex"
+
+
+
+
+const hero_ul1 = document.getElementById('hero_ul1');
+
+hero_ul1.style.listStyle = 'none';
+hero_ul1.style.fontSize = '31px';
+hero_ul1.style.fontFamily = 'Nunito Sans';
+hero_ul1.style.fontWeight = '700';
+hero_ul1.style.display = 'flex';
+hero_ul1.style.flexDirection = 'column';
+hero_ul1.style.gap = '16px';
+
+
+
+
+const hero_ul2 = document.getElementById('hero_ul2');
+
+hero_ul2.style.listStyle = 'none';
+hero_ul2.style.fontSize = '31px';
+hero_ul2.style.fontFamily = 'Nunito Sans';
+hero_ul2.style.fontWeight = '700';
+hero_ul2.style.display = 'flex';
+hero_ul2.style.flexDirection = 'column';
+hero_ul2.style.gap = '16px';
+
+
+
+
+
+
+
+// team design qismi
+const team_des = document.getElementById('team_design');
+
+team_des.style.display ="flex";
+team_des.style.gap ="88px";
+team_des.style.alignItems ="center";
+team_des.style.marginBottom ="81px";
+
+
+const t_d_h3 = document.getElementById('team_des_h3');
+
+t_d_h3.style.color = '#2B2B2B';
+t_d_h3.style.fontSize = '25px';
+t_d_h3.style.fontFamily = 'Nunito Sans';
+t_d_h3.style.maxWidth = '272.19px';
+t_d_h3.style.marginBottom = '20px';
+
+
+const t_d_p = document.getElementById('team_des_p');
+
+t_d_p.style.fontFamily = 'Nunito Sans';
+t_d_p.style.color = '#2B2B2B';
+t_d_p.style.maxWidth = '272.19px';
+t_d_p.style.fontWeight = '600';
+
+
+const t_d_i = document.getElementById('team_des_img');
+
+t_d_i.style.display = 'flex';
+t_d_i.style.gap = '59.92px';
+
+
+
+
+
+// nulla qismi
+
+const nulla_h1 = document.getElementById('nulla_h1');
+
+nulla_h1.style.textAlign = 'center';
+nulla_h1.style.fontFamily = 'Nunito Sans';
+nulla_h1.style.fontSize = '56px';
+nulla_h1.style.color = '#2B2B2B';
+nulla_h1.style.marginBottom = '81px';
+
+
+
+const nulla_s =document.getElementById('nulla_smaller');
+
+nulla_s.style.display = 'flex';
+nulla_s.style.gap = '95px';
+nulla_s.style.marginLeft = '119px';
+nulla_s.style.marginRight = '119px';
+nulla_s.style.marginBottom = '97px';
+
+
+
+const nulla_smallest1 = document.getElementById('nulla_smallest1');
+
+nulla_smallest1.style.padding = '60px 24px 28px 60px';
+nulla_smallest1.style.boxShadow = '0 0 40px #3928A626';
+
+const nulla_p1 =document.getElementById('nulla_p1');
+
+nulla_p1.style.color = '#2B2B2B';
+nulla_p1.style.fontSize = '16px';
+nulla_p1.style.fontWeight = '600';
+nulla_p1.style.fontFamily = 'Nunito Sans';
+
+
+
+
+const nulla_smallest2 = document.getElementById('nulla_smallest2');
+
+nulla_smallest2.style.padding = '60px 14px 28px 60px';
+nulla_smallest2.style.boxShadow = '0 0 40px #3928A626';
+
+const nulla_p2 =document.getElementById('nulla_p2');
+
+nulla_p2.style.color = '#2B2B2B';
+nulla_p2.style.fontSize = '16px';
+nulla_p2.style.fontWeight = '600';
+nulla_p2.style.fontFamily = 'Nunito Sans';
+
+
+
+
+const nulla_h5_1 = document.getElementById('nulla_h5_1');
+
+nulla_h5_1.style.fontFamily = 'Nunito Sans';
+nulla_h5_1.style.fontSize = '18px';
+nulla_h5_1.style.color = '#2B2B2B';
+nulla_h5_1.style.marginTop = '20px';
+nulla_h5_1.style.lineHeight = '28px';
+nulla_h5_1.style.marginBottom = '0px';
+
+const nulla_smallest_p1 = document.getElementById('nulla_smallest_p1');
+
+nulla_smallest_p1.style.fontFamily = 'Nunito Sans';
+nulla_smallest_p1.style.fontSize = '16px';
+nulla_smallest_p1.style.color = '#2B2B2B';
+nulla_smallest_p1.style.fontWeight = '600';
+nulla_smallest_p1.style.margin = '0';
+
+
+
+
+const nulla_h5_2 = document.getElementById('nulla_h5_2');
+
+nulla_h5_2.style.fontFamily = 'Nunito Sans';
+nulla_h5_2.style.fontSize = '18px';
+nulla_h5_2.style.color = '#2B2B2B';
+nulla_h5_2.style.marginTop = '20px';
+nulla_h5_2.style.lineHeight = '28px';
+nulla_h5_2.style.marginBottom = '0px';
+
+const nulla_smallest_p2 = document.getElementById('nulla_smallest_p2');
+
+nulla_smallest_p2.style.fontFamily = 'Nunito Sans';
+nulla_smallest_p2.style.fontSize = '16px';
+nulla_smallest_p2.style.color = '#2B2B2B';
+nulla_smallest_p2.style.fontWeight = '600';
+nulla_smallest_p2.style.margin = '0';
+
+
+
+
+
+
+// foooter
+
+const footer = document.getElementById('footer');
+
+footer.style.display = 'flex';
+footer.style.justifyContent = 'space-between';
+
+
+const footer_h6 = document.getElementById('footer_h6');
+
+footer_h6.style.fontSize = '14px';
+footer_h6.style.fontFamily = 'Nunito Sans';
+footer_h6.style.color = '#2B2B2B';
+footer_h6.style.marginBottom = '20.29px';
+
+
+const footer_icons = document.getElementById('footer_icons');
+
+footer_icons.style.display = 'flex';
+footer_icons.style.flexDirection = 'row';
+footer_icons.style.gap = '20px';
+footer_icons.style.marginBottom = '23.06px';
+
+
+
+const footer_h5 = document.getElementById('footer_h5');
+
+footer_h5.style.fontSize = '14px';
+footer_h5.style.fontFamily = 'Nunito Sans';
+footer_h5.style.color = '#2B2B2B';
+footer_h5.style.maxWidth = '182.41px';
+footer_h5.style.fontWeight = '600';
+footer_h5.style.margin = '0';
